da306ded6c1ecaccb3a075c8374c7a507e4bc49e
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-09-19 16:37:00 +0200

    Added re auth route to api, and created the login route with passport

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import testRoute from "./test";
+import authRoute from "./auth";
 const router = Router();
 
 router.use("/test", testRoute);
+router.use("/auth", authRoute);
 
 export default router;
--- src/strategies/local.ts
+++ src/strategies/local.ts
@@ -1,5 +1,28 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
+import UserSchema, { User } from "../database/schemas/Users";
+import { CompareHash } from "../utils/helpers/hashCredentials";
+
+passport.serializeUser((user: any, done) => {
+    console.log("Serializing User")
+    console.log(user);
+    done(null, user.id);  //req.sessions.passport.user
+})
+
+passport.deserializeUser(async (id: string, done) => {
+    console.log("Deserializing user");
+
+    try {
+        const userDB = await UserSchema.findById<User>(id);
+
+        if (!userDB) throw Error("User not found");
+        console.log(userDB);
+        done(null, userDB);  //req.user
+    } catch (err) {
+        console.log(err);
+        done(err, null);
+    }
+})
 
 passport.use(
     new Strategy(
@@ -7,9 +30,27 @@ passport.use(
             usernameField: "email",
         },
         async (email, password, done) => {
-            if (!email || password) throw Error("Missing credentials");
 
-            const userDB = await 
+            try {
+                if (!email || !password) throw Error("Missing credentials");
+
+                const userDB = await UserSchema.findOne({ email })
+
+                if (!userDB) throw Error("User not found!")
+
+                const isValid = CompareHash(password, userDB.password)
+
+                if (isValid) {
+                    console.log("Auth sucessfull");
+                    done(null, userDB);
+                } else {
+                    console.log("Invalid Auth");
+                    done(null, null);
+                }
+            } catch (err) {
+                console.log(err);
+                done(err, null);
+            }
         }
     )
 )
